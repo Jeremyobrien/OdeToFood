c4dd63b4a06d2c7235b233637f6a3207d90f9c5f
@Jeremyobrien <jjob1391@gmail.com>
2022-08-15 15:25:06 -0400

    Inital commit

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood.Pages;
+
+public class AboutModel : PageModel
+
+{
+    private string _message { get; set; }
+
+    public string Message { get; set; }
+
+    public AboutModel(string _message)
+    {
+        Message = _message;
+    }
+
+    public string OnGet()
+    {
+        return Message;
+    }
+}
+
---
+++ Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
